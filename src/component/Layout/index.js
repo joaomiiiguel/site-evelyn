a64ea9aa6695ebf7492bc6675e32c6d24e001e7e
João Miguel <macbookprom@Macbooks-MacBook-Pro.local>
2023-05-16 23:35:41 -0300

    configurar o analytics

--- src/component/Layout/index.js
+++ src/component/Layout/index.js
@@ -8,25 +8,7 @@ export default function Layout({ title, children }) {
     <div>
       <Head>
         <title>{title}</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name="description" content="Cientista social, Coach de vida e carreira, especialista em felicidade e palestrante. Trabalho direcionando mulheres a construção de uma vida próspera, abundante e feliz onde a realização pessoal e profissional acontecem simultaneamente." />
-        <meta name="keywords" content="Coaching, Palestra, Vendas, Treinamentos, Palestrante, Promoção, Crescimento, profissional, Carreira" />
-        <meta name="author" content="João Miguel" />
-        <meta property="og:image" content="https://www.evelynpinheiro.com.br/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fabout.ee99ce1b.webp&w=640&q=75" data-head-react="true" />
-
       </Head>
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
-          ga('create', 'G-260RCJD2QS', 'auto');
-          ga('send', 'pageview');
-        `}
-      </Script>
-      <Script
-        src="https://www.google-analytics.com/analytics.js"
-        strategy="afterInteractive"
-      />
       <div className='flex flex-col justify-center items-center'>
         {children}
         <Footer />
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,12 @@ export default function Document() {
   return (
     <Html lang="pt-br">
       <Head>
+      <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Cientista social, Coach de vida e carreira, especialista em felicidade e palestrante. Trabalho direcionando mulheres a construção de uma vida próspera, abundante e feliz onde a realização pessoal e profissional acontecem simultaneamente." />
+        <meta name="keywords" content="Coaching, Palestra, Vendas, Treinamentos, Palestrante, Promoção, Crescimento, profissional, Carreira" />
+        <meta name="author" content="João Miguel" />
+        <meta property="og:image" content="https://www.evelynpinheiro.com.br/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fabout.ee99ce1b.webp&w=640&q=75" data-head-react="true" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet" />
