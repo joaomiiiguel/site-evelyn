52399c1e64e788a2de466b67819dfabe95b8a6a9
João Miguel <macbookprom@Macbooks-MacBook-Pro.local>
2023-05-18 17:10:49 -0300

    config activehosted

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,7 +6,8 @@ export default function App({ Component, pageProps }) {
     <>
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-260RCJD2QS"
-        strategy="afterInteractive" /><Script id="google-analytics" strategy="afterInteractive">
+        strategy="afterInteractive" />
+      <Script id="google-analytics" strategy="afterInteractive">
         {`
         window.dataLayer = window.dataLayer || [];
         function gtag(){window.dataLayer.push(arguments);}
@@ -14,6 +15,11 @@ export default function App({ Component, pageProps }) {
 
         gtag('config', 'G-260RCJD2QS');
       `}
+      </Script>
+      <Script id='activehosted' type="text/javascript">
+        {
+        `(function (e, t, o, n, p, r, i) { e.visitorGlobalObjectAlias = n; e[e.visitorGlobalObjectAlias] = e[e.visitorGlobalObjectAlias] || function () { (e[e.visitorGlobalObjectAlias].q = e[e.visitorGlobalObjectAlias].q || []).push(arguments) }; e[e.visitorGlobalObjectAlias].l = (new Date).getTime(); r = t.createElement("script"); r.src = o; r.async = true; i = t.getElementsByTagName("script")[0]; i.parentNode.insertBefore(r, i) })(window, document, "https://diffuser-cdn.app-us1.com/diffuser/diffuser.js", "vgo"); vgo('setAccount', '26861870');vgo('setTrackByDefault', true);vgo('process')`
+        }
       </Script>
       <Component {...pageProps} />
     </>
