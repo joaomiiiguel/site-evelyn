47f9260a472a3eed9019142b8baf79be7c820ffd
João Miguel <macbookprom@Macbooks-MacBook-Pro.local>
2023-05-25 12:07:52 -0300

    fix image meta

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,7 +9,7 @@ export default function Document() {
         <meta name="description" content="Cientista social, Coach de vida e carreira, especialista em felicidade e palestrante. Trabalho direcionando mulheres a construção de uma vida próspera, abundante e feliz onde a realização pessoal e profissional acontecem simultaneamente." />
         <meta name="keywords" content="Coaching, Palestra, Vendas, Treinamentos, Palestrante, Promoção, Crescimento, profissional, Carreira" />
         <meta name="author" content="João Miguel" />
-        <meta property="og:image" content="https://www.evelynpinheiro.com.br/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fpalestras.e4303dd1.webp&w=828&q=75" data-head-react="true" />
+        <meta property="og:image" content="https://www.evelynpinheiro.com.br//_next/static/media/capaMeta.cf0627e8.webp" data-head-react="true" />
         <meta property="og:image:width" content="400" />
         <meta property="og:image:height" content="200" />
         <meta property="og:url" content="https://www.evelynpinheiro.com.br/" />
